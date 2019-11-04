7cd26590209586dac57088b51325338d5d93eaef
williamschwindt <33535511+williamschwindt@users.noreply.github.com>
2019-11-04 13:55:12 -0800

    getRunnersByTShirtSize works in repl

--- index.js
+++ index.js
@@ -224,8 +224,11 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  let sizes = runners.filter(function(person) {
+    return person.shirt_size === tShirtSize;
+  });
+  return sizes.map(shirtSize => `${shirtSize.first_name}, ${shirtSize.last_name}`);
 }
 
 /**
